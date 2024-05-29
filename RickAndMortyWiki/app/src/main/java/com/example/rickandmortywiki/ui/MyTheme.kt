faa2d8de4595f6975bddf67636dbc8900bff7363
Feodor Maslov <f-maslov@bk.ru>
2024-05-29 15:57:42 +0300

    deleted

--- RickAndMortyWiki/app/src/main/java/com/example/rickandmortywiki/ui/MyTheme.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rickandmortywiki.ui
-
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-
-@Composable
-fun MyTheme(content: @Composable () -> Unit) {
-    MaterialTheme(content = content)
-}
